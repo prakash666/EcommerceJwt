6411b80b82408219e6e8a92fa5fa2c470d420dd8
Prakash Dangi <dangip1000@gmail.com>
2023-08-20 18:50:55 -0400

    This is first commit of the project

---
+++ src/main/java/com/security/EcommerceSecurity/EcommerceSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security.EcommerceSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/EcommerceSecurity/EcommerceSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.EcommerceSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
